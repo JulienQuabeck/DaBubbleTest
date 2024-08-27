631a05fd1af3ae8505f9b90a7027d9370f39c49d
unknown <j.quabeck@gmx.net>
2024-08-27 11:36:47 +0200

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,11 +5,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatCardModule, MatInputModule, MatDividerModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatCardModule, MatInputModule, MatDividerModule, MatButtonModule, DashboardComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
