4f9fe733ff997de7541b043f7cddd6db1eb8ee24
unknown <j.quabeck@gmx.net>
2024-08-28 23:14:10 +0200

    Channels

--- src/app/dashboard-channels/dashboard-channels.component.ts
+++ src/app/dashboard-channels/dashboard-channels.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard-channels',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './dashboard-channels.component.html',
   styleUrl: './dashboard-channels.component.scss'
 })
