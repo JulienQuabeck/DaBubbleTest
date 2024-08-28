27a1b012083dd1059c85a5ac5d6b2834155eb8f8
unknown <j.quabeck@gmx.net>
2024-08-28 14:00:54 +0200

    working on dashboard

--- src/app/dashboard-header/dashboard-header.component.ts
+++ src/app/dashboard-header/dashboard-header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-dashboard-header',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatMenuModule],
   templateUrl: './dashboard-header.component.html',
   styleUrl: './dashboard-header.component.scss'
 })
